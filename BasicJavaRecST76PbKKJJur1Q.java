1ec2d23fc297d3dd19b7daea47b1177cd0e6f7fb
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2023-04-02 19:17:20 +0300

    done home work

---
+++ BasicJavaRecST76PbKKJJur1Q.java
@@ -0,0 +1,12 @@
+public class BasicJavaRecST76PbKKJJur1Q {
+    public static void main(String[] args) {
+
+        char start = '9';
+
+        System.out.println(Integer.parseInt(String.valueOf(start)));     // the first method
+        System.out.println(Integer.parseInt(String.valueOf(start)) + 1); // check the first method
+
+        System.out.println(Character.getNumericValue(start));     // the second method
+        System.out.println(Character.getNumericValue(start) + 1); // check the second method
+    }
+}
